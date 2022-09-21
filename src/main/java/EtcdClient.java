96dbe99fe5f8a9459a5864b969cd86fed21c7858
chrishperera <pererachrishan7@gmail.com>
2022-09-21 20:33:06 +0530

    initial commit

---
+++ src/main/java/EtcdClient.java
@@ -0,0 +1,10 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Chrishan Perera
+ * Date: 9/21/2022
+ * Time: 8:26 PM
+ */
+public class EtcdClient {
+}
---
+++ src/main/java/NameServiceClient.java
@@ -0,0 +1,10 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Chrishan Perera
+ * Date: 9/21/2022
+ * Time: 8:30 PM
+ */
+public class NameServiceClient {
+}
